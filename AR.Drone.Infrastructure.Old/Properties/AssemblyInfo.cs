ca4665f862adb1c9b4f1fe7b79dcd0935223bd38
Jeppe Beier <jeppevinkel@gmail.com>
2025-03-26 22:10:51 +0100

    Create dotnet 8 AR.Drone.Infrastructure

--- AR.Drone.Infrastructure.Old/Properties/AssemblyInfo.cs
+++ AR.Drone.Infrastructure.Old/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AR.Drone.Infrastructure")]
+[assembly: AssemblyTitle("AR.Drone.Infrastructure.Old")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AR.Drone.Infrastructure")]
+[assembly: AssemblyProduct("AR.Drone.Infrastructure.Old")]
 [assembly: AssemblyCopyright("Copyright © Ruslan Balanukhin 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
