5d687585be92ea4a4de2b27180eb63d8edfb2086
Jeppe Beier <jeppevinkel@gmail.com>
2025-03-26 21:49:17 +0100

    Move project to new name Project is renamed to allow space for a new project to replace it. Doing an in-place migration was too tedious, so I decided to re-create it side-by-side instead.

--- AR.Drone.Data.Old/Properties/AssemblyInfo.cs
+++ AR.Drone.Data.Old/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AR.Drone.Data")]
+[assembly: AssemblyTitle("AR.Drone.Data.Old")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AR.Drone.Data")]
+[assembly: AssemblyProduct("AR.Drone.Data.Old")]
 [assembly: AssemblyCopyright("Copyright © Ruslan Balanukhin 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
